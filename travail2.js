12cde730b33d456e96bce781031ec97798134615
Klevin Mutombo <100364481+Klevin777@users.noreply.github.com>
2022-04-22 13:58:31 +0000

    bare things fam

--- travail2.js
+++ travail2.js
@@ -1,16 +1,24 @@
 function soumet(){
 j = 0 
+
+  var nom = document.getElementById("txtNom").value
+  if (nom == ""){
+    erreur = ("Vous devez entrer votre nom!")
+    var erreur2 = document.getElementById("erreur")
+    erreur2.innerHTML = erreur
+  }
+  
   var checkvariable = document.getElementsByName("variable");
   var messagevariable = "";
      
-        if (checkvariable[0].checked) {
+        if (checkvariable[0].checked && nom != "") {
         messagevariable += " " + checkvariable[0].value;
           j = j + 1 
           var q1 = ("Bonne réponse!")
 var qu1 = document.getElementById("bon");
 qu1.innerHTML = q1
               }
-  else {
+  else if (nom != "") {
     rep1 = ("La bonne reponse est ParseInt :(")
     var repo = document.getElementById("mauvais");
     repo.innerHTML = rep1
@@ -21,14 +29,14 @@ qu1.innerHTML = q1
   
   var checkvalise = document.getElementsByName("charAt");
     var valisechoisi = "";
-        if (checkvalise[0].checked && !checkvalise[1].checked) {
+        if (checkvalise[0].checked && !checkvalise[1].checked && nom != "") {
             valisechoisi = checkvalise[0].value;
           j = j + 1
           var q2 = ("Bonne réponse!");
           var qu2 = document.getElementById("bonne");
           qu2.innerHTML = q2
               }
-  else {
+  else if (nom !="") {
     rep2 = ("La bonne reponse est charAt :(")
     var repo2 = document.getElementById("mauvais2");
     repo2.innerHTML = rep2
@@ -37,22 +45,24 @@ qu1.innerHTML = q1
           
   
 var optionvraifaux = document.getElementById("selvraioufaux").value;
-        if (optionvraifaux == "vrai"){
+        if (optionvraifaux == "vrai" && nom !=""){
          j = j + 1 
           var q3 = ("Bonne réponse!");
           var qu3 = document.getElementById("bonne1");
           qu3.innerHTML = q3
             }
-  else{
+  else if (nom !=""){
     rep3 = ("La bonne reponse est vrai :(")
     var repo3 = document.getElementById("mauvais3");
     repo3.innerHTML = rep3
   }
-  
- 
-var score = ("Votre resultat est" + " " + j  + " sur 3")
+
+
+ if (nom != ""){
+var score = (nom + " votre resultat est" + " " + j  + " sur 3")
 var score2 = document.getElementById("affichescore");
-score2.innerHTML = score
+score2.innerHTML = score}
+  
 }
   
   
